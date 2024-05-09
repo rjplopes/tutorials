b816c9cee465dadc3c4c758d3efccbd588e90f0a
rjplopes <rjp.lopes@gmail.com>
2024-05-09 12:18:23 +0100

    Advanced C# FactoryPattern and Delegates

---
+++ source/DelegateBasicExample/DelegateBasicExample/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace DelegateBasicExample
+{
+	internal class Program
+	{
+
+		delegate void LogDel(string text);
+		static void Main(string[] args)
+		{
+			Log log = new Log();
+
+			LogDel LogTextToScreenDel, LogTexTtoFileDel;
+
+			LogTextToScreenDel = new LogDel(log.LogTextToScreen);
+
+			LogTexTtoFileDel = new LogDel(log.LogTextToFile);
+
+			LogDel multiLogDel = LogTextToScreenDel + LogTexTtoFileDel;
+
+			Console.WriteLine("Please enter your name");
+
+			var name = Console.ReadLine();
+
+			LogText(LogTexTtoFileDel, name);
+
+			Console.ReadKey();
+		}
+
+		static void LogText(LogDel logDel, string text)
+		{
+			logDel(text);
+		}
+
+		
+	}
+
+	public class Log
+	{
+		public void LogTextToScreen(string text)
+		{
+			Console.WriteLine($"{DateTime.Now}: {text}");
+
+		}
+
+		public void LogTextToFile(string text)
+		{
+			using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log.txt"), true))
+			{
+				sw.WriteLine($"{DateTime.Now}: {text}");
+			}
+		}
+	}
+}
